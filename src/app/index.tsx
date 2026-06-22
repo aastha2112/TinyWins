b77df40ae2779e4f95a487e871aafff3423359b0
Aastha <aasthalakhanpal11@gmail.com>
2026-06-23 00:22:04 +0530

    chore: adding navigation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,23 @@
+import HomeScreen from "@/screens/HomeScreen";
 import OnboardingScreen from "@/screens/OnboardingScreen";
-import { Stack } from "expo-router";
+import { NavigationContainer } from "expo-router/build/react-navigation";
+import { createNativeStackNavigator } from "expo-router/build/react-navigation/native-stack";
 import { StyleSheet, View } from "react-native";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Stack.Screen options={{ headerShown: false }} />
-      <OnboardingScreen />
+      {/* <Stack.Screen options={{ headerShown: false }} />
+      <OnboardingScreen /> */}
+
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={OnboardingScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,49 @@
-import { Text } from "react-native";
+import { createNativeStackNavigator } from "expo-router/build/react-navigation/native-stack";
+import { useState } from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { FlatList } from "react-native-reanimated/lib/typescript/Animated";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const HomeScreen = () => {
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
+  const [winText, setWinText] = useState("");
+  const [tinyWins, setTinyWins] = useState([
+    { id: 1, win: "Drink Water" },
+    { id: 2, win: "jogging" },
+    { id: 3, win: "running" },
+    { id: 4, win: "Study" },
+    { id: 5, win: "Music" },
+  ]);
+
+  const handleAddTinyWin = (winText: string) => {
+    return [...tinyWins, { id: Date.now(), win: winText }];
+  };
+
+  const WinsList = () => {
+    return (
+      <FlatList
+        data={tinyWins}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity onPress={() => navigation.navigate("Details")}>
+              <Text>{item.win}</Text>
+              <Text>{"->"}</Text>
+            </TouchableOpacity>
+          );
+        }}
+      />
+    );
+  };
+
   return (
     <SafeAreaView>
       <Text>Home Screen</Text>
+
+      {/* now */}
+
+      {/* now */}
     </SafeAreaView>
   );
 };
