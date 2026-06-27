9945e3dd3a0bcd60bc7258d7604f7b048553e1bc
Aastha <aasthalakhanpal11@gmail.com>
2026-06-27 13:49:45 +0530

    feat: designed onboarding screen 2 and 3

--- src/app/onboarding/FinalScreen.tsx
+++ src/app/onboarding/FinalScreen.tsx
@@ -1,15 +1,34 @@
 import { useRouter } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import LogoGlow from "../../../assets/images/logo-glow.png";
 
 const FinalScreen = () => {
+  console.log("final");
   const router = useRouter();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Final Screen</Text>
-      <Pressable onPress={() => router.replace("/Home")}>
-        <Text style={styles.text}>Next</Text>
-      </Pressable>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>You've Got This !</Text>
+        <Text style={styles.subtitle}>
+          Let's start celebrating your journey together. Remember, your tiny
+          wins matter.
+        </Text>
+        <Text style={styles.subtitle}>Keep Going.</Text>
+
+        <Image source={LogoGlow} style={styles.logo} />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Pressable
+          onPress={() => router.replace("/Home")}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Get Started</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -17,11 +36,48 @@ export default FinalScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    flex: 0.9,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: 15,
+    paddingVertical: 50,
+    paddingHorizontal: 22,
+  },
+  title: {
+    color: "black",
+    fontSize: 28,
+    fontWeight: "800",
+    width: "80%",
+    textAlign: "center",
+  },
+  subtitle: {
+    color: "#666",
+    fontSize: 15,
+    fontWeight: "400",
+    textAlign: "center",
+    width: "80%",
+  },
+  logo: {
+    width: 450,
+    height: 450,
+    marginTop: 20,
+    resizeMode: "contain",
+  },
+  buttonContainer: {
+    paddingBottom: 20,
     alignItems: "center",
   },
-  text: {
-    color: "red",
+  button: {
+    width: "60%",
+    backgroundColor: "pink",
+    paddingVertical: 12,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
--- src/app/onboarding/SecondScreen.tsx
+++ src/app/onboarding/SecondScreen.tsx
@@ -1,15 +1,32 @@
 import { useRouter } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import LogoGlow from "../../../assets/images/logo-glow.png";
 
 const SecondScreen = () => {
   const router = useRouter();
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Second Screen</Text>
-      <Pressable onPress={() => router.push("/onboarding/FinalScreen")}>
-        <Text style={styles.text}>Next</Text>
-      </Pressable>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Small Wins, Big Impact</Text>
+        <Text style={styles.subtitle}>
+          Build momentum, one task at a time. It's about showing up, not doing
+          it all.
+        </Text>
+        <Text style={styles.subtitle}>Celebrate showing up today.</Text>
+
+        <Image source={LogoGlow} style={styles.logo} />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Pressable
+          onPress={() => router.push("/onboarding/FinalScreen")}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Next</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -17,11 +34,48 @@ export default SecondScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    flex: 0.9,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: 15,
+    paddingVertical: 50,
+    paddingHorizontal: 22,
+  },
+  title: {
+    color: "black",
+    fontSize: 28,
+    fontWeight: "800",
+    width: "80%",
+    textAlign: "center",
+  },
+  subtitle: {
+    color: "#666",
+    fontSize: 15,
+    fontWeight: "400",
+    textAlign: "center",
+    width: "80%",
+  },
+  logo: {
+    width: 450,
+    height: 450,
+    marginTop: 20,
+    resizeMode: "contain",
+  },
+  buttonContainer: {
+    paddingBottom: 20,
     alignItems: "center",
   },
-  text: {
-    color: "red",
+  button: {
+    width: "60%",
+    backgroundColor: "pink",
+    paddingVertical: 12,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
